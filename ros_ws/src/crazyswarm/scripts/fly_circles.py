c491a42108c6a3ff4c7c9ceaf9201ef972d149ee
Mauro Pfister <mauro.pfister@epfl.ch>
2020-12-02 12:44:19 +0100

    Cleanup fly_circles script

--- ros_ws/src/crazyswarm/scripts/fly_circles.py
+++ ros_ws/src/crazyswarm/scripts/fly_circles.py
@@ -2,54 +2,45 @@
 
 import numpy as np
 from pycrazyswarm import *
-import csv
-import time
 
-Z = 0.5 # Takeoff height
 
 if __name__ == "__main__":
     swarm = Crazyswarm()
     timeHelper = swarm.timeHelper
     allcfs = swarm.allcfs
 
-    allcfs.setParam("stabilizer/controller", 2)  # Set controller
-    
-    # Reset kalman filer by setting kalman/resetEstimation to 1 and back to 0
-    allcfs.setParam("kalman/resetEstimation", 1)  # Set controller
-    allcfs.setParam("kalman/resetEstimation", 0)  # Set controller
-
-    allcfs.takeoff(targetHeight=Z, duration=1.0+Z)
-    timeHelper.sleep(1.5+Z)
+    # Parameters
+    Z = 0.5  # Takeoff height
+    r = 0.75  # Circle radius
+    w = 2 * np.pi / 8  # Angular velocity
+    t = 0
+    dt = 0.1  # Time discretization
 
     mode = 'cmdFullState'  # 'cmdFullState' or 'cmdPosition'
 
-    cmd_pos_list = []
-    meas_pos_list = []
-    time_list = []
-    r = 0.75
-    w = 2 * np.pi / 8
-    t = 0
-    dt = 0.1
+    allcfs.setParam("stabilizer/controller", 2)  # Set controller
+    
+    # Reset kalman filer by setting kalman/resetEstimation to 1 and back to 0
+    allcfs.setParam("kalman/resetEstimation", 1)
+    allcfs.setParam("kalman/resetEstimation", 0)
 
+    # Takeoff
+    allcfs.takeoff(targetHeight=Z, duration=1.0+Z)
+    timeHelper.sleep(1.5+Z)
+
+    # Move crazyflies to position on circle
     for cf in allcfs.crazyflies:
         pos = np.array([r * np.cos(w * t), r * np.sin(w * t), Z])
         cf.goTo(cf.initialPosition + pos, 0, 2)
     timeHelper.sleep(2)
 
-
-    t0 = time.time()
-
+    # Fly in circles
     while t < 8:
         pos = np.array([r * np.cos(w * t), r * np.sin(w * t), Z])
         vel = np.array([-r * w * np.sin(w * t), r * w * np.cos(w * t), 0])
         acc = np.array([-r * w * w * np.cos(w * t), -r * w * w * np.sin(w * t), 0])
         yaw = w * t
         omega = np.array([0, 0, w])
-
-        time_list.append(timeHelper.time())
-        cmd_pos_list.append(cf.initialPosition + pos)
-        meas_pos_list.append(cf.position())
-
 
         for cf in allcfs.crazyflies:
             if mode == 'cmdFullState':
@@ -62,27 +53,12 @@ if __name__ == "__main__":
         t += dt
         timeHelper.sleepForRate(1 / dt)
 
-    t1 = time.time()
-    print(t1- t0)
-
+    # Move crazyflies back to initial position
     for cf in allcfs.crazyflies:
         cf.notifySetpointsStop(remainValidMillisecs=100)
         cf.goTo(cf.initialPosition + np.array([0, 0, Z]), 0, 2)
-
     timeHelper.sleep(2)
 
+    # Land
     allcfs.land(targetHeight=0.02, duration=2.0+Z)
     timeHelper.sleep(1.0+Z)
-
-    file_name = 'test_' + mode + '.csv'
-    with open(file_name, 'w') as file:
-        writer = csv.writer(file)
-        cmd_pos_array = np.array(cmd_pos_list)
-        meas_pos_array = np.array(meas_pos_list)
-        writer.writerow(time_list)
-        writer.writerow(cmd_pos_array[:, 0])
-        writer.writerow(cmd_pos_array[:, 1])
-        writer.writerow(cmd_pos_array[:, 2])
-        writer.writerow(meas_pos_array[:, 0])
-        writer.writerow(meas_pos_array[:, 1])
-        writer.writerow(meas_pos_array[:, 2])
